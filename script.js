f02a3f383a0fbc077ded73bb8d67af0273f40017
Andriy Andryeyev <a.andryeyev@spd-ukraine.com>
2021-02-26 11:49:33 +0200

    run in --compatibility-mode=base; update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-import http from 'k6/http';
-import { check } from 'k6';
+var http = require('k6/http');
+var check = require('k6').check;
 
-export let options = {
-  thresholds: {
-    http_req_duration: ['avg < 200', 'p(99)<1000', 'p(99.999)<1000'], 
-  },
+exports.options = {
+    thresholds: {
+        http_req_duration: ['avg < 200', 'p(99)<1000', 'p(99.999)<1000'],
+    },
 };
 
-export default function () {
-  var res = http.get('http://pong/ping/k6');
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-    'is duration <= 1000ms': (r) => r.http_req_duration < 1000
-  });
+exports.default = function () {
+    var resp = http.get('http://pong/ping/k6');
+    var ok = check(resp, {
+        'is status 200': function (r) { return r.status === 200; },
+        'is http_req_receiving >= 0': function (r) { return r.timings.receiving >= 0; },
+    });
+    if (!ok) {
+        console.log(JSON.stringify(resp, null, 4));
+    }
 }
